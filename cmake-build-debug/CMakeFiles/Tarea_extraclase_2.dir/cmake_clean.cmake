b5acb5159e9e32b5f3e53de29222d50c69c8aadf
Marco <markorovi24@gmail.com>
2021-03-25 21:11:07 -0600

    first commit

---
+++ cmake-build-debug/CMakeFiles/Tarea_extraclase_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_extraclase_2.dir/main.cpp.obj"
+  "Tarea_extraclase_2.exe"
+  "Tarea_extraclase_2.exe.manifest"
+  "Tarea_extraclase_2.pdb"
+  "libTarea_extraclase_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_extraclase_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
